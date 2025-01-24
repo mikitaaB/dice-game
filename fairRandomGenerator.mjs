0ac68ff4fc2f6f7cfc210fcdd1beaae4c0c2517d
Mikita Buinavets <mikita.buinavets@arua.co>
2025-01-24 19:53:54 +0300

    Changed implementation of generateRandomNumber func

--- fairRandomGenerator.mjs
+++ fairRandomGenerator.mjs
@@ -1,7 +1,6 @@
-import { randomBytes, createHmac } from 'node:crypto';
+import { randomBytes, createHmac, randomInt } from 'node:crypto';
 
 const RANDOM_BYTES_LENGTH = 32;
-const RANDOM_BUFFER_LENGTH = 4;
 
 const generateSecretRandomKey = () => {
     return randomBytes(RANDOM_BYTES_LENGTH);
@@ -14,9 +13,7 @@ const calculateHMAC = (value, secret) => {
 }
 
 const generateRandomNumber = (max) => {
-    const randomBuffer = randomBytes(RANDOM_BUFFER_LENGTH);
-    const randomNumber = randomBuffer.readUInt32BE(0);
-    return randomNumber % max;
+    return randomInt(max);
 }
 
 const generateFairRandomValue = (max) => {
